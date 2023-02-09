0af7c429818cb8f8629290b756f9ddd77849925d
variskurbanov <abduk1910@mail.ru>
2023-02-09 16:03:12 +0600

    hw3

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,17 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        double[] numbers = {7.5, 7.0, 8.1, 8.2, -5.9, 5.5, 23.2, 23.1, 16.3, 4.4, 34.7, 21.1, 3.3, 2.2, 4.4};
+        double[] numbers = {7.5, 7.0, 8.1, 8.2, -1.1, 22, 23.2, -3.0, 16.3, 4.4, -3.0, 1.1, 3.3, 2.2, -4.4,-55};
         double summaChisel = 0;
         int kolichestvoChisel = 0;
+        boolean fall = false;
         for (double num : numbers) {
-            if (num > 0) {
+            if (num < 0) {
+                fall = true;
+            } else if (num > 0 && fall) {
                 summaChisel = summaChisel + num;
                 kolichestvoChisel++;
-            } else {
-                summaChisel = 0;
-                kolichestvoChisel = 0;
             }
-
         }
         double result = summaChisel / kolichestvoChisel;
         System.out.println(result);
